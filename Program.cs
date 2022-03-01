b34eaef10268079beb183e35d719bad8a1d5cdaf
Jackson Duarte <jacksonwillerduarte@gmail.com>
2022-03-01 16:14:16 -0300

    startup extensions

--- Program.cs
+++ Program.cs
@@ -1,11 +1,4 @@
 using StarupClass.net6;
 
-var builder = WebApplication.CreateBuilder(args);
-
-var startup =  new Startup(builder.Configuration);
-startup.ConfigurationService(builder.Services);
-
-var app = builder.Build();
-
-startup.Configure(app, app.Environment);
-app.Run();
+var builder = WebApplication.CreateBuilder(args)
+    .UseStartup<Startup>();
--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 namespace StarupClass.net6;
 
-public class Startup{
+public class Startup : IStartup
+{
 
     public Startup(IConfiguration configuration)
     {
@@ -31,6 +32,33 @@ public class Startup{
         app.UseAuthorization();
 
         app.MapControllers();
-            }
+    }
+}
+
+public interface IStartup
+{
+    IConfiguration Configuration { get; }
+    void Configure(WebApplication app, IWebHostEnvironment environment);
+    void ConfigurationService(IServiceCollection services);
+}
+
+public static class StartupExtensions
+{
+    public static WebApplicationBuilder UseStartup<TStartup>
+    (this WebApplicationBuilder WebAppBuilder)  
+    where TStartup : IStartup
+        {
+            var startup = Activator.CreateInstance(typeof(TStartup),WebAppBuilder.Configuration) as IStartup;
+            if (startup == null) throw new ArgumentException("Class Startup.cs inválida!");
+
+            startup.ConfigurationService(WebAppBuilder.Services);
+            var app = WebAppBuilder.Build();
+            startup.Configure(app, app.Environment);
+
+            app.Run();
+
+            return WebAppBuilder;
+
+        }
 }
 
