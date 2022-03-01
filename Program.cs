82e77085ce0d653c5f7707b2482c51910ba7506a
Jackson Duarte <jacksonwillerduarte@gmail.com>
2022-03-01 15:29:01 -0300

    standard startup

---
+++ Program.cs
@@ -0,0 +1,11 @@
+using StarupClass.net6;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var startup =  new Startup(builder.Configuration);
+startup.ConfigurationService(builder.Services);
+
+var app = builder.Build();
+
+startup.Configure(app, app.Environment);
+app.Run();
---
+++ Startup.cs
@@ -0,0 +1,36 @@
+namespace StarupClass.net6;
+
+public class Startup{
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigurationService(IServiceCollection services)
+    {
+
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+    }
+
+    public void Configure(WebApplication app, IWebHostEnvironment environment)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+            }
+}
+
